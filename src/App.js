5cfd77f168a85baeda48691fbaac6bcd5827fb82
gustavo Almeida <gu.almeidan2007@gmail.com>
2025-02-04 08:58:08 -0300

    Add routing and initial page structure for RPG master helper

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Initiative from './pages/Initiative';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>RPG master helper</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/initiative" element={<Initiative />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Initiative.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Initiative() {
+  return (
+    <div>
+      <h1>Página de Initiative</h1>
+    </div>
+  );
+}
+
+export default Initiative; 
